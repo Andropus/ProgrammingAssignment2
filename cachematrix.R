3aff417facd558d36045ea485c12c3b5c2872013
Andropus <45640302+Andropus@users.noreply.github.com>
2020-06-09 14:16:11 -0700

    Assignment 2: Lexical scoping in R
    Here's my version of two functions, intended to store in the cache an inverted matrix and check, whether it has been already calculated or not.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions show the possibilities provided with so-called "closures",
+#i.e., functions, written by other functions within themselves.
 
-## Write a short comment describing this function
+
+## This function creates a "matrix"-like cache object, which is capable of 
+## storing the inverted version of itself
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## Setting the "default" (not exactly) values for input matrix
+    inversion <- NULL
+    set <- function(y) {
+        x <<- y
+        inversion <<- NULL
+    }
+    ## Getting the input matrix
+    get <- function() x
+    ## Inverting the input matrix
+    setInverse <- function() inversion <<- solve(x)
+    ## Returning the inverted matrix
+    getInverse <- function() {
+        inversion
+    }
+    ## Creating a list of parameters
+    list(set = set,get = get,setInverse = setInverse,getInverse = getInverse)  
 }
 
-
-## Write a short comment describing this function
+## This function checks whether the cache of matrix inversion from the previous
+## step is still exists, and, if so, return the CASHED (non-recomputed) value
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inversion <- x$getInverse()
+    ## Checking whether matrix has been already computed and it has no changes
+    if (!is.null(inversion)) {
+        message("getting cached matrix")
+        return(inversion)
+    }
+    ## Getting the matrix
+    matrix <- x$get()
+    ## Inverting and setting the inverted matrix
+    inversion <- solve(matrix, ...)
+    x$setInverse(inversion)
+    ## Return inverted matrix
+    inversion
 }
